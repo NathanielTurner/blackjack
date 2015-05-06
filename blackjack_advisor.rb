638c2b68d2fb105202e61a389a09595b4000f6ac
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-06 14:18:11 -0400

    Adds some initial framework

---
+++ blackjack_advisor.rb
@@ -0,0 +1,22 @@
+#checks a given value and turns it into a coresponding integer or makes the user
+#enter a new value if the value entered isnt allowed than returns the value
+def optimizer(given)
+  optimized = false
+end
+
+#adds the two cards and returns thier total
+def adder(first, second)
+  total = 0
+  return total
+end
+
+
+
+moves = {}
+
+puts "Please enter your first card:"
+first_card = gets.chomp
+puts "Please tner your second card:"
+second_card = gets.chomp
+puts "Please enter the dealer's card:"
+dealers_card = gets.chomp
